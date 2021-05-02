1ceb5b3516ffc887fff083f93cd715a12f7b7b1f
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-05-02 17:58:29 +0200

    elvileg minden jó

--- public/game.mjs
+++ public/game.mjs
@@ -1,6 +1,151 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
+import controls from './controls.mjs';
+import { generateStartPos, canvasCalcs } from './canvas-data.mjs';
+import { post } from '../server';
 
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+const loadImg = () => {
+  const img = new Image();
+  img.src = src;
+  return img;
+};
+
+const bronzeCoinArt = loadImg("https://cdn.freecodecamp.org/demo-projects/images/bronze-coin.png");
+const silverCoinArt = loadImg("https://cdn.freecodecamp.org/demo-projects/images/silver-coin.png");
+const goldCoinArt = loadImg("https://cdn.freecodecamp.org/demo-projects/images/gold-coin.png");
+const mainPlayerArt = loadImg("https://cdn.freecodecamp.org/demo-projects/images/main-player.png");
+const otherPlayerArt = loadImg("https://cdn.freecodecamp.org/demo-projects/images/other-player.png");
+
+let tick;
+let currentPlayers = [];
+let item;
+let endGame;
+
+socket.on("init", ({ id, players, coin }) => {
+  console.log(`Connected ${id}`);
+
+  cancelAnimationFrame(tick);
+
+  const mainPlayer = new Player({
+    x: generateStartPos(
+      canvasCalcs.playFieldMinX,
+      canvasCalcs.playFieldMaxX,
+      5
+    ),
+    y: generateStartPos(
+      canvasCalcs.playFieldMinY,
+      canvasCalcs.playFieldMaxY,
+      5
+    ),
+    id,
+    main: true
+  });
+
+  controls(mainPlayer, socket);
+
+  socket.emit("new-player", mainPlayer);
+
+  socket.on("new-player", (obj) => {
+    const playerIds = currentPlayers.map((player) => player.id);
+    if (!playerIds.includes(obj.id)) {
+      currentPlayers.push(new Player(obj));
+    }
+  });
+
+  socket.on("move-player", ({ id, dir, posObj }) => {
+    const movingPlayer = currentPlayers.find((obj) => obj.id === id);
+    movingPlayer.moveDir(dir);
+
+    movingPlayer.x = posObj.x;
+    movingPlayer.y = posObj.y;
+  });
+
+  socket.on("stop-player", ({ id, dir, posObj }) => {
+    const stoppingPlayer = currentPlayers.find((obj) => obj.id === id);
+    stoppingPlayer.stopDir(dir);
+
+    stoppingPlayer.x = posObj.x;
+    stoppingPlayer.y = posObj.y;
+  });
+
+  socket.on("new-coin", (newCoin) => {
+    item = new Collectible(newCoin);
+  });
+
+  socket.on("remove-player", (id) => {
+    console.log(`${id} disconnected`);
+    currentPlayers = currentPlayers.filter((player) => player.id !== id);
+  });
+
+  socket.on("end-game", (result) => (endGame = result));
+
+  socket.on("update-player", (playerObj) => {
+    const scoringPlayer = currentPlayers.find((obj) => obj.id === playerObj.id);
+    scoringPlayer.score = playerObj.score;
+  });
+
+  currentPlayers = players.map((val) => new Player(val)).concat(mainPlayer);
+  item = new Collectible(coin);
+
+  draw();
+});
+
+const draw = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  //Background
+  context.fillStyle = "#220";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  //Border for playfield
+  context.strokeStyle = "white";
+  context.strokeRect(
+    canvasCalcs.playFieldMinX,
+    canvasCalcs.playFieldMinY,
+    canvasCalcs.playFieldWidth,
+    canvasCalcs.playFieldHeight
+  );
+
+  // Controls text
+  context.fillStyle = "white";
+  context.font = `13px 'Press Start 2P'`;
+  context.textAlign = "center";
+  context.fillText("Controls: WASD", 100, 32.5);
+
+  // Game title
+  context.font = `16px 'Press Start 2P'`;
+  context.fillText("Coin Race", canvasCalcs.canvasWidth / 2, 32.5);
+
+  // Calculate score and draw players each frame
+  currPlayers.forEach((player) => {
+    player.draw(context, item, { mainPlayerArt, otherPlayerArt }, currPlayers);
+  });
+
+  // Draw current coin
+  item.draw(context, { bronzeCoinArt, silverCoinArt, goldCoinArt });
+
+  // Remove destroyed coin
+  if (item.destroyed) {
+    socket.emit("destroy-item", {
+      playerId: item.destroyed,
+      coinValue: item.value,
+      coinId: item.id,
+    });
+  }
+
+  if (endGame) {
+    context.fillStyle = "white";
+    context.font = `13px 'Press Start 2P'`;
+    context.fillText(
+      `You ${endGame}! Restart and try again.`,
+      canvasCalcs.canvasWidth / 2,
+      80
+    );
+  }
+
+  if (!endGame) tick = requestAnimationFrame(draw);
+};
--- server.js
+++ server.js
@@ -3,11 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expect = require('chai');
 const socket = require('socket.io');
+const helmet = require('helmet');
+const nocache = require('nocache');
+const cors = require('cors');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
 
 const app = express();
+
+app.use(helmet({
+  noSniff: true,
+  xssFilter: true,
+  hidePoweredBy: {
+    setTo: 'PHP 7.4.3'
+  }
+}));
+
+app.use(nocache);
+
+app.use(cors({ origin: '*' }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
@@ -47,6 +62,112 @@ const server = app.listen(portNum, () => {
       }
     }, 1500);
   }
+});
+
+// Socket.io setup:
+// Start and bind to the same port
+
+const io = socket(server);
+const Collectible = require('./public/Collectible');
+const { generateStartPosition, canvasCalcs } = require('./public/canvas-data');
+
+let currentPlayers = [];
+const destroyedCoins = [];
+
+const generateCoin = () => {
+  const rand = Math.random();
+  let coinValue;
+
+  if (rand < 0.6) {
+    coinValue = 1;
+  } else if (rand < 0.85) {
+    coinValue = 2;
+  } else {
+    coinValue = 3;
+  }
+
+  return new Collectible({
+    x: generateStartPosition(
+      canvasCalcs.playFieldMinX,
+      canvasCalcs.playFieldMaxX,
+      5
+    ),
+    y: generateStartPosition(
+      canvasCalcs.playFieldMinY,
+      canvasCalcs.playFieldMaxX,
+      5
+    ),
+    value: coinValue,
+    id: Date.now()
+  });
+};
+
+let coin = generateCoin();
+
+io.sockets.on('connection', (socket) => {
+  console.log(`New connection ${socket.id}`);
+
+  socket.emit('init', { id: socket.id, players: currentPlayers, coin });
+
+  socket.on("new-player", (obj) => {
+    obj.id = socket.id;
+    currentPlayers.push(obj);
+    socket.broadcast.emit("new-player", obj);
+  });
+
+  socket.on("move-player", (dir, obj) => {
+    const movingPlayer = currentPlayers.find((player) => player.id === socket.id);
+    if(movingPlayer) {
+      movingPlayer.x = obj.x;
+      movingPlayer.y = obj.y;
+
+      socket.broadcast.emit("move-player", {
+        id: socket.id,
+        dir,
+        posObj: { x: movingPlayer.x, y: movingPlayer.y }
+      });
+    }
+  });
+
+  socket.on("stop-player", (dir, obj) => {
+    const stoppingPlayer = currentPlayers.find((player) => player.id === socket.id);
+
+    if (stoppingPlayer) {
+      stoppingPlayer.x = obj.x;
+      stoppingPlayer.y = obj.y;
+
+      socket.broadcast.emit("stop-player", {
+        id: socket.id,
+        dir,
+        posObj: { x: stoppingPlayer.x, y: stoppingPlayer.y }
+      });
+    }
+  });
+
+  socket.on("destroy-item", ({ playerId, coinValue, coinId }) => {
+    if (!destroyedCoins.includes(coinId)) {
+      const scoringPlayer = currentPlayers.find((obj) => obj.id === playerId);
+      const sock = io.sockets.connected[scoringPlayer.id];
+
+      scoringPlayer.score += coinValue;
+      destroyedCoins.push(coinId);
+
+      io.emit("update-player", scoringPlayer);
+
+      if (scoringPlayer.score >= 100) {
+        sock.emit("end-game", "win");
+        sock.broadcast.emit("end-game", "lose");
+      }
+
+      coin = generateCoin();
+      io.emit("new-coin", coin);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("remove-player", socket.id);
+    currentPlayers = currentPlayers.filter((player) => player.id !== socket.id);
+  });
 });
 
 module.exports = app; // For testing
